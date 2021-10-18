c32f0205bb9d882722c1b8f4ebe5088ee22529a8
Rodrigo Luque <rodrigoluquepuig@gmail.com>
2021-10-18 20:25:21 -0300

    Ejercicio 1

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,27 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"luke.jpg");
+
+            PipeNull pipeNull = new PipeNull();
+
+            IPicture image2 = pipeNull.Send(picture);
+
+            FilterNegative filterNegative = new FilterNegative();
+            FilterGreyscale filterGreyscale = new FilterGreyscale();
+
+            PipeSerial pipeSerial2 = new PipeSerial(filterNegative, pipeNull);
+
+            IPicture image1 = pipeSerial2.Send(image2);
+
+            PipeSerial pipeSerial1 = new PipeSerial(filterGreyscale, pipeSerial2);
+
+            IPicture image = pipeSerial1.Send(image1);
+
+            PictureProvider provider_end = new PictureProvider();
+            provider_end.SavePicture(image, @"luke2.jpg");
+            
         }
     }
 }
